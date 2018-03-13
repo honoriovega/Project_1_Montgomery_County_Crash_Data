f852ee7d521d76cce0d5f3a70008a4837ccfa4b7
Honorio Vega <hvega@csumb.edu>
2018-03-13 15:01:01 -0700

    added exploration.R

---
+++ exploration.R
@@ -0,0 +1,11 @@
+# Team Members : Honorio Vega, Jaime Velasquez
+# Date: 03/13/2018
+# CST383 - Data Science - Dr. Bruns
+
+#https://docs.google.com/document/d/e/2PACX-1vTS7PeYktuRQ3cHLZD6iFstYzMoaHoLWqQ2MJcvBcq5fZkbCw3wuQpPrv8fIvlc-4UZQBDzDuAvKIng/pub
+
+# Read data from the disk
+dat = read.csv('Crash_Reporting_-_Drivers_Data.csv')
+
+# print the data
+print(dat)
